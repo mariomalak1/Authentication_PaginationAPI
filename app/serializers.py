7e957665c669fd6df55818fae026b232848eec0a
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-07-05 16:58:39 +0300

    add email in registration

--- app/serializers.py
+++ app/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-class Login_Serializer(serializers.Serializer):
+class Registration_Serializer(serializers.Serializer):
 	username = serializers.CharField(max_length=150)
 	password = serializers.CharField(max_length=150)
+	email = serializers.EmailField(required=False)
 
 	def validate_username(self, username):
 		user = User.objects.filter(username=username).first()
 		if user:
 			raise serializers.ValidationError("This Username Is Already Taken Before")
 		return username
+
+	def validate_email(self, email):
+		user = User.objects.filter(email=email).first()
+		if user:
+			raise serializers.ValidationError("This Email Is Already Taken Before")
+		return email
--- app/views.py
+++ app/views.py
@@ -2,16 +2,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 from django.contrib.auth.models import User
 
-from .serializers import Login_Serializer
+from .serializers import Registration_Serializer
 # Create your views here.
 
 @api_view(["POST"])
 def registration(request):
 	data = request.data
-	serializer = Login_Serializer(data=data)
+	serializer = Registration_Serializer(data=data)
 	if serializer.is_valid():
 		user = User.objects.create(username=serializer.data.get("username"))
 		user.set_password(serializer.data.get("password"))
+		if serializer.data.get("email"):
+			user.email = serializer.data.get("email")
 		user.save()
 		return Response(serializer.data, status=200)
 	else:
